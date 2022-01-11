5a801ab392558a430ab4843f1b660c13effea7aa
xfrnk2 <xfrnk2@gmail.com>
2022-01-11 19:11:59 +0900

    feat: 사이트 목록 내 해당하는 경로의 웹 페이지를 스크래핑 하고, 원하는 데이터를 추출하는 기능 구현

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
-import csv
-from urllib import urlopen
+from dataclasses import dataclass
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
 
 CSV_PATH = "..\\resources\\Site1.csv"
 URL_INDEX = 1
 
-def main():
-    pass
+
+@dataclass
+class Course:
+    href: str
+    title: str
+    instructor: str
+
 
 def read_urls_from_csv(path):
     url_list = []
@@ -14,8 +21,40 @@ def read_urls_from_csv(path):
         line = f.readline()
         url = line.split(",")[URL_INDEX]
         url_list.append(url)
-        print(url)
     return url_list
 
+
+def scrap_category(url_list):
+    result = []
+    for url in url_list:
+        url_without_new_line = url[:-1]
+        u = urlopen(url)
+        soup = BeautifulSoup(u.read(), 'html.parser')
+        raw_result = soup.find_all("div", {"class": "course_card_item"})
+        result.append(scrap_course(url_without_new_line, raw_result))
+    return result
+
+
+def scrap_course(url, raw_category_data):
+    result = []
+    for data in raw_category_data:
+        href = url + data.find("a")["href"]
+        info = data.find("div", {"class": "card-content"})
+        title = info.find("div", {"class": "course_title"}).get_text()
+        instructor = info.find("div", {"class": "instructor"}).get_text()
+        result.append(Course(href, title, instructor))
+    return result
+
+
+def print_result(category_data):
+    for course_data in category_data:
+        for course in course_data:
+            print(f"title: {course.title}\n"
+                  f"instructor: {course.instructor}\n"
+                  f"href: {course.href}\n")
+
+
 if __name__ == "__main__":
-    url_list = read_urls_from_csv(CSV_PATH)
+    urls = read_urls_from_csv(CSV_PATH)
+    result = scrap_category(urls)
+    print_result(result)
