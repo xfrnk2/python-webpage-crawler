ce885333c5daa53ddb9ef2cf22de286df516e6f3
xfrnk2 <xfrnk2@gmail.com>
2022-01-11 19:31:35 +0900

    refactor: 데이터 서빙 자료형을 딕셔너리로 대체 등
    - 상수 추가 및 명확한 이름으로 정정

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 CSV_PATH = "..\\resources\\Site1.csv"
-URL_INDEX = 1
+CATEGORY_NAME_INDEX = 0
+CATEGORY_URL_INDEX = 1
 
 
 @dataclass
@@ -14,24 +15,27 @@ class Course:
     instructor: str
 
 
-def read_urls_from_csv(path):
-    url_list = []
+def read_info_from_csv(path):
+    data = {}
     f = open(path, 'r')
     for _ in f:
         line = f.readline()
-        url = line.split(",")[URL_INDEX]
-        url_list.append(url)
-    return url_list
+        row = line.split(",")
+        name = row[CATEGORY_NAME_INDEX]
+        url = row[CATEGORY_URL_INDEX]
+
+        data[name] = url
+    return data
 
 
-def scrap_category(url_list):
-    result = []
-    for url in url_list:
+def scrap_category(info):
+    result = {}
+    for name, url in info.items():
         url_without_new_line = url[:-1]
         u = urlopen(url)
         soup = BeautifulSoup(u.read(), 'html.parser')
         raw_result = soup.find_all("div", {"class": "course_card_item"})
-        result.append(scrap_course(url_without_new_line, raw_result))
+        result[name] = scrap_course(url_without_new_line, raw_result)
     return result
 
 
@@ -42,19 +46,24 @@ def scrap_course(url, raw_category_data):
         info = data.find("div", {"class": "card-content"})
         title = info.find("div", {"class": "course_title"}).get_text()
         instructor = info.find("div", {"class": "instructor"}).get_text()
-        result.append(Course(href, title, instructor))
+        result.append({
+            "href": href,
+            "title": title,
+            "instructor": instructor,
+        })
     return result
 
 
 def print_result(category_data):
-    for course_data in category_data:
+    for course_data in category_data.values():
         for course in course_data:
-            print(f"title: {course.title}\n"
-                  f"instructor: {course.instructor}\n"
-                  f"href: {course.href}\n")
+            print(f"title: {course['title']}\n"
+                  f"instructor: {course['instructor']}\n"
+                  f"href: {course['href']}\n")
 
 
 if __name__ == "__main__":
-    urls = read_urls_from_csv(CSV_PATH)
-    result = scrap_category(urls)
+    info = read_info_from_csv(CSV_PATH)
+    result = scrap_category(info)
     print_result(result)
+    
