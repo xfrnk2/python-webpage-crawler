4badd5132f53a9358160c6fa40144a0c68b99ce9
xfrnk2 <xfrnk2@gmail.com>
2022-01-11 18:13:31 +0900

    refactor: csv 파일 내부 데이터 중 URL만을 추출하고, 반환 가능하도록 수정
    - 역할에 어울리는 이름으로 함수명 수정

--- src/main.py
+++ src/main.py
@@ -1,14 +1,21 @@
-CSV_PATH = "..\\resources\\Site1.csv"
+import csv
+from urllib import urlopen
 
+CSV_PATH = "..\\resources\\Site1.csv"
+URL_INDEX = 1
 
 def main():
     pass
 
-def read_csv_file(path):
+def read_urls_from_csv(path):
+    url_list = []
     f = open(path, 'r')
-    for line in f:
-        print(f.readline())
+    for _ in f:
+        line = f.readline()
+        url = line.split(",")[URL_INDEX]
+        url_list.append(url)
+        print(url)
+    return url_list
 
 if __name__ == "__main__":
-    print("hi")
-    read_csv_file(CSV_PATH)
+    url_list = read_urls_from_csv(CSV_PATH)
